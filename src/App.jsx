faff7b1c21ea7a07f6604b281e752dc034b940f0
Bikram Rai <bikramdevlopment@gmail.com>
2024-09-16 07:58:05 +0545

    UI changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-
-import './App.css'
+import "./App.css";
 import WeatherApp from "./WeatherApp.jsx";
 function App() {
-  
   return (
     <>
-        
-<WeatherApp />
-<br />
-<br />
-<br />
-<span >Build and Develop by <a style={{textDecoration:"none"}}href="https://bikramrai-portfolio.vercel.app/">Bikram Rai</a></span>
-
+      <WeatherApp />
+      <br />
+      <br />
+      <br />
+      <span>
+        Build and Develop by{" "}
+        <a
+          style={{ textDecoration: "none" }}
+          href="https://bikramrai-portfolio.vercel.app/"
+        >
+          Bikram Rai
+        </a>
+      </span>
     </>
-  )
+  );
 }
 
 export default App;
--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,29 +1,35 @@
 import SearchBox from "./SearchBox.jsx";
 import InfoBox from "./InfoBox.jsx";
-import {useState} from "react";
-export default function WeatherApp () {
-    const [weatherInfo, setWeatherInfo] = useState({
-        city:"Kathmandu",
-        feelsLike: 30.93,
-        humidity: 58,
-        temp: 29.12,
-        tempMax: 29.12,
-        tempMin: 25.12,
-        weather: "broken clouds",
-    });
+import { useState } from "react";
+export default function WeatherApp() {
+  const [weatherInfo, setWeatherInfo] = useState({
+    // city:"Kathmandu",
+    // feelsLike: 30.93,
+    // humidity: 58,
+    // temp: 29.12,
+    // tempMax: 29.12,
+    // tempMin: 25.12,
+    // weather: "broken clouds",
+  });
 
+  let updateInfo = (result) => {
+    setWeatherInfo(result);
+  };
 
-    let updateInfo = (result) => {
-        setWeatherInfo(result);
-        }
-
-
-    return (
-        <div >
-            <h2>Weather App by <a style={{color:"black", textDecoration:"none"}} href="https://bikramrai-portfolio.vercel.app/">Bikram Rai</a></h2>
-            <SearchBox updateInfo={updateInfo} />
-            <br />
-            <InfoBox info={weatherInfo}/>
-        </div>
-    )
+  return (
+    <div>
+      <h2>
+        Weather App by{" "}
+        <a
+          style={{ color: "black", textDecoration: "none" }}
+          href="https://bikramrai-portfolio.vercel.app/"
+        >
+          Bikram Rai
+        </a>
+      </h2>
+      <SearchBox updateInfo={updateInfo} />
+      <br />
+      <InfoBox info={weatherInfo} />
+    </div>
+  );
 }
